6e6056a96469e69c0136688046a79d9c4cbdba17
dell <9716588439vik@gmail.com>
2016-08-31 10:54:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/movingimageview/SampleActivity.java
@@ -0,0 +1,86 @@
+package com.example.dell.movingimageview;
+
+import android.animation.Animator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.dell.mymovie.view.MovingImageView;
+
+
+public class SampleActivity extends AppCompatActivity {
+
+    MovingImageView image;
+    boolean toggleState = true;
+    boolean toggleCustomMovement = true;
+    int[] imageList = {R.drawable.anotherworld, R.drawable.futurecity, R.drawable.spacecargo, R.drawable.city};
+    int pos = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        image = (MovingImageView) findViewById(R.id.image);
+        image.getMovingAnimator().addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                Log.i("Sample MovingImageView", "Start");
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Log.i("Sample MovingImageView", "End");
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                Log.i("Sample MovingImageView", "Cancel");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                Log.i("Sample MovingImageView", "Repeat");
+            }
+        });
+    }
+
+    public void clickImage(View v) {
+        if (toggleState) {
+            image.getMovingAnimator().pause();
+            Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
+        } else {
+            image.getMovingAnimator().resume();
+            Toast.makeText(this, "Resume", Toast.LENGTH_SHORT).show();
+        }
+        toggleState = !toggleState;
+    }
+
+    public void clickTitle(View v) {
+        pos = (pos + 1) >= imageList.length ? 0 : pos + 1;
+        image.setImageResource(imageList[pos]);
+        toggleCustomMovement = true;
+        Toast.makeText(this, "Next picture", Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickText(View v) {
+        if(toggleCustomMovement) {
+            image.getMovingAnimator().addCustomMovement().addDiagonalMoveToDownRight().addHorizontalMoveToLeft().addDiagonalMoveToUpRight()
+                    .addVerticalMoveToDown().addHorizontalMoveToLeft().addVerticalMoveToUp().start();
+            Toast.makeText(this, "Custom movement", Toast.LENGTH_SHORT).show();
+        } else {
+            image.getMovingAnimator().clearCustomMovement();
+            Toast.makeText(this, "Default movement", Toast.LENGTH_SHORT).show();
+        }
+        toggleCustomMovement = !toggleCustomMovement;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(image != null)
+            image.getMovingAnimator().cancel();
+    }
+}
